119814a9ca6158e6b7c0dcb12c250df53e29d1ee
Josh Sullivan <jaysche.sullivan@gmail.com>
2012-07-28 21:06:56 -0400

    Initial commit, yo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaylorPortfolio::Application.config.secret_token = '93fd33308ad3483cb59b044cc6d6d709b75233fcd0d4aaa8de82466eef0e24b9dd099058d64220ffad6464516b62427bf33ea34d07482a4e9ca5761b62f398ff'
